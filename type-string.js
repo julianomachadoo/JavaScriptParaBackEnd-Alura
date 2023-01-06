8c63c70e9c4adff5deb9ab3d3a60a612265ca5e3
julianomachadoo <julmachadoramos@gmail.com>
2023-01-06 14:16:06 -0300

    tipo string

---
+++ type-string.js
@@ -0,0 +1,15 @@
+const texto1 = "Olá mundo!";
+const texto2 = 'Olá mundo';
+
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Juliano";
+console.log(citacao + meuNome);
+
+// concatenação (+)
+
+
+
+// template string OU template literal
