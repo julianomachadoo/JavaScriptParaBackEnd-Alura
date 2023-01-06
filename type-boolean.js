8cf7776c70f96265a030a96942ebe78e3df3109f
julianomachadoo <julmachadoramos@gmail.com>
2023-01-06 14:34:35 -0300

    tipo boolean

---
+++ type-boolean.js
@@ -0,0 +1,13 @@
+// boolean
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+
+console.log(primeiroNumero === segundoNumero);
+
+// Comapracao com strings
+
+const texto1 = "Alura";
+const texto2 = "a";
+
+console.log(texto1 === texto2);
