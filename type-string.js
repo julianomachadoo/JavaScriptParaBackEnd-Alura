5138fdfb181988a2094200d5bbae8b8bd89f29b3
julianomachadoo <julmachadoramos@gmail.com>
2023-01-06 14:22:14 -0300

    um pouco mais sobre strings

--- type-string.js
+++ type-string.js
@@ -4,12 +4,37 @@ const texto2 = 'Olá mundo';
 const senha = "senhaSuperSegura456!";
 const StringDeNumeros = "34567";
 
+
+// Concatenação (+)
+
 const citacao = "Meu nome é ";
 const meuNome = "Juliano";
 console.log(citacao + meuNome);
 
-// concatenação (+)
 
+// Código Unicode
 
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
 
-// template string OU template literal
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+// Trabalhando com strings
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+console.log(cidade === input); // false
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
+
+const minhaSenha = "minhaSenha123";
+console.log(senha.length) // 13 caracteres
+
